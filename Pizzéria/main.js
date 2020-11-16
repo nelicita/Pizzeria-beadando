649d19c43fea98721d9b9badb327675b5e5727a7
nelicita <70948373+nelicita@users.noreply.github.com>
2020-11-16 22:50:46 +0100

    Add files via upload

---
+++ Pizzéria/main.js
@@ -0,0 +1,9 @@
+let penz = 0;
+function Ar1(){
+    penz +=1350;
+    document.getElementById("kosarid").innerHTML = "Kosár: " +penz + "Ft";
+}
+function Ar2(){
+    penz +=1800;
+    document.getElementById("kosarid").innerHTML = "Kosár: "+penz + "Ft";
+}
